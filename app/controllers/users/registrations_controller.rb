e05cef3e5bb582ad6e7ae056bc1784dd4ea07726
Eddie Kao <eddie@5xruby.tw>
2019-09-15 20:00:53 +0800

    avatar upload

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -47,7 +47,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :intro])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :intro, :avatar])
   end
 
   def update_resource(resource, params)
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :stories
   validates :username, presence: true, uniqueness: true
+  has_one_attached :avatar
 end
