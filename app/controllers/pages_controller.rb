1a9bf6915af179a78d999cad2b1046b03f137619
Eddie Kao <eddie@5xruby.tw>
2019-09-15 20:45:18 +0800

    add index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,12 @@
 class PagesController < ApplicationController
 
   def index
+    @stories = Story.order(created_at: :desc).includes(:user)
   end
 
+  def show
+  end
+
+  def user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,11 @@ Rails.application.routes.draw do
 
   resources :stories
 
+  # /@kaochenlong/文章標題-123
+  get '@:username/:story_id', to: 'pages#show', as: 'story_page'
+
+  # /@kaochenlong/
+  get '@:username', to: 'pages#user', as: 'user_page'
+
   root 'pages#index'
 end
