38c7d85154b5571530ecbb906bfaa0e563033335
Eddie Kao <eddie@5xruby.tw>
2019-11-15 05:09:41 +0800

    [feature] clap - part 2

--- app/frontend/controllers/story_controller.js
+++ app/frontend/controllers/story_controller.js
@@ -7,16 +7,22 @@ export default class extends Controller {
   addClap(event) {
     event.preventDefault()
     let slug = event.currentTarget.dataset.slug
+    let target = this.clapCountTarget
 
     axios.post(`/stories/${slug}/clap`)
          .then(function(response){
-           console.log(response.data)
+           let status = response.data.status
+           switch (status) {
+             case 'sign_in_first':
+               alert('你必須先登入')
+               break
+             default:
+               target.innerHTML = status
+           }
          })
          .catch(function(error){
            console.log(error)
          })
-       
-    // this.clapCountTarget.innerHTML = 'CCH'
   }
 }
 
