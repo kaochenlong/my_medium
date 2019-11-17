1b2d54c797999249aefbd899543338ea29bc608d
Eddie Kao <eddie@5xruby.tw>
2019-11-18 03:29:11 +0800

    [feature] editor - part 2

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   }
 
   namespace :api do
+    post :upload_image, to: 'utils#upload_image'
+
     resources :users, only: [] do
       member do
         post :follow
